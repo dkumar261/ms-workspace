d7e29d70eeebb482cdfbee9895f1a64ce280a96d
Dinesh Kumar <dinesh.prasad@collinsongroup.com>
2023-01-18 04:09:54 +0800

    First Commit.

--- rest-webservice/src/main/java/com/practice/ms/webservice/RestWebserviceApplication.java
+++ rest-webservice/src/main/java/com/practice/ms/webservice/RestWebserviceApplication.java
@@ -2,8 +2,10 @@ package com.practice.ms.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RestWebserviceApplication {
 
 	public static void main(String[] args) {
