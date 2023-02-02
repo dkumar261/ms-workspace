b8975b0c0ced0f6fadd7db7f526496a46653fad1
Dinesh Kumar <dinesh.prasad@collinsongroup.com>
2023-02-02 22:09:50 +0530

    First Commit.

---
+++ ms-card/src/main/java/com/practice/ms/webservice/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.practice.ms.webservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+	
+}
---
+++ ms-card/src/main/java/com/practice/ms/webservice/util/CardServiceConstants.java
@@ -0,0 +1,6 @@
+package com.practice.ms.webservice.util;
+
+public class CardServiceConstants {
+
+	public static final String GREETING = "/ws/v1";
+}
