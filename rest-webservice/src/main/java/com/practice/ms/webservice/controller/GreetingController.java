7ba65b9fc79c8cb99514a945e389c22703fbea29
Dinesh Kumar <dinesh.prasad@collinsongroup.com>
2023-02-20 23:09:19 +0800

    Adding readme file.

--- rest-webservice/src/main/java/com/practice/ms/webservice/controller/GreetingController.java
+++ rest-webservice/src/main/java/com/practice/ms/webservice/controller/GreetingController.java
@@ -3,12 +3,16 @@ package com.practice.ms.webservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.ms.webservice.domain.Greeting;
+import com.practice.ms.webservice.model.GreetingModel;
 import com.practice.ms.webservice.service.GreetingService;
 import com.practice.ms.webservice.util.ApplicationConstants;
+
 /**
  * 
  * @author Dinesh Kumar
@@ -21,10 +25,16 @@ public class GreetingController {
 	@Autowired
 	private GreetingService greetingService;
 
-	@GetMapping(value = "/greeting/{id}")
-	public Greeting greeting(@PathVariable("id") Integer id) {
-		Greeting greeting = greetingService.getGreetings(id);
+	@GetMapping(value = "/item/{itemId}/article/{articleId}")
+	public GreetingModel greeting(@PathVariable("articleId") Integer id, @PathVariable("itemId") Integer typeId) {
+		GreetingModel greeting = greetingService.getGreetings(id, typeId);
 		return greeting;
 	}
-	
+
+	@PostMapping(value = "/item")
+	public Greeting add(@RequestBody Greeting greeting) {
+		Greeting savedGreeting = greetingService.save(greeting);
+		return savedGreeting;
+	}
+
 }
