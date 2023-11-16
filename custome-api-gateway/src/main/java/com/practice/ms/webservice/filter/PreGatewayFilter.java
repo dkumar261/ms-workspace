0d599b6324d5f17afb50acff75ccac481cc1e894
dineshnextq <v.dinesh.kumar@nextq.ai>
2023-11-17 00:34:21 +0530

    api gateway feature.

--- custome-api-gateway/src/main/java/com/practice/ms/webservice/filter/PreGatewayFilter.java
+++ custome-api-gateway/src/main/java/com/practice/ms/webservice/filter/PreGatewayFilter.java
@@ -1,7 +1,5 @@
 package com.practice.ms.webservice.filter;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
