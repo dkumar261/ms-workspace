4629278f2d9c72b7497d5ba86b53e4325d485cd5
dineshnextq <v.dinesh.kumar@nextq.ai>
2023-11-25 22:05:09 +0530

    Adding new functionality.

--- user-setting/src/main/java/com/user/setting/service/UserService.java
+++ user-setting/src/main/java/com/user/setting/service/UserService.java
@@ -1,12 +1,13 @@
 package com.user.setting.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import com.user.setting.dao.UserDao;
+import com.github.dozermapper.core.Mapper;
 import com.user.setting.domain.User;
 import com.user.setting.model.UserModel;
 import com.user.setting.repository.UserRepository;
@@ -15,27 +16,20 @@ import com.user.setting.repository.UserRepository;
 public class UserService {
 
 	@Autowired
-	private UserDao userDao;
-
-	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private PasswordEncoder passwordEncoder;
-
-	public List<UserModel> getUsers() {
-
-		return userDao.getUsers();
-	}
-
-	public UserModel getUserIdName(String name) {
-		UserModel user = userDao.getUserIdName(name);
-		return user;
-	}
+	
+	@Autowired
+	private Mapper mapper;
 
 	public UserModel createUser(UserModel userModel) {
-		String encode = passwordEncoder.encode(userModel.getPassword());
-		User user = User.builder().name(userModel.getName()).password(encode).build();
+		
+		User user = mapper.map(userModel, User.class);
+		String encodedPassword = passwordEncoder.encode(userModel.getPassword());
+		user.setPassword(encodedPassword);
+		user.setCreated(LocalDateTime.now());
 		User savedUser = userRepository.save(user);
 
 		UserModel returedUserModel = UserModel.builder().id(savedUser.getId()).name(savedUser.getName()).build();
